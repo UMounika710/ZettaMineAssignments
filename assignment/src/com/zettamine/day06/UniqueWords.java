5ccfe85acf2789d6b0b6acafd851d85451ae3a61
Mounika <Mounika@ZML-LP57>
2023-12-13 22:15:45 +0530

    day-6

---
+++ assignment/src/com/zettamine/day06/UniqueWords.java
@@ -0,0 +1,30 @@
+package com.zettamine.day06;
+
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class UniqueWords {
+
+	public static void main(String[] args) {
+		Scanner sc  = new Scanner(System.in); 
+		System.out.println("Enter student's article ");
+		String article = sc.nextLine();
+		String[] words = article.split("[,;:.?!\\s]+");
+		System.out.println("Number of words " + words.length);
+		
+		Set<String> uniqueWords = new TreeSet<>();
+		
+		for(String word : words) {
+			uniqueWords.add(word.toLowerCase());
+		}
+		System.out.println("Number of unique words " + uniqueWords.size());
+		System.out.println("The words are :");
+		for(String word : uniqueWords) {
+			System.out.println(word);
+		}
+		sc.close();
+
+	}
+
+}
